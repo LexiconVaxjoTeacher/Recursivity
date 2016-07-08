cb6040b2a4c90700d0150b137f545781a0fef16c
Eric Lindroth <ericl87@hotmail.com>
2016-07-08 10:28:26 +0200

    Added a bit of code (X+Y)

--- recursivity/recursivity/Program.cs
+++ recursivity/recursivity/Program.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace recursivity {
+namespace Recursivity {
 	class Program {
 		static void Main(string[] args) {
+			string path = @"C:\Intel\";
+			string directoryPath = Path.GetDirectoryName(path);
+			GetChildren(directoryPath);
+		}
+
+		static void GetChildren(string path) {
+			Console.WriteLine(path);
+			string[] paths = Directory.GetDirectories(path);
+			foreach(var childPath in paths) {
+				GetChildren(childPath);
+			}
 		}
 	}
+}
 }
